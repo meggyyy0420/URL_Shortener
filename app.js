8e21cd1988182219e28f87ef061eec9b8140b6fe
amberrr0420 <amberrr0420@gmail.com>
2021-10-04 17:41:20 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/URL_Shortener')
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hi')
