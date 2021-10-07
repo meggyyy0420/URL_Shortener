80e2ad882d3cee44048a3b71ad3e5e2a1da75375
amberrr0420 <amberrr0420@gmail.com>
2021-10-07 12:29:58 +0800

    no message

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect('mongodb://localhost/URL_Shortener')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -23,9 +13,7 @@ app.set('view engine', 'hbs')
 // 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000.')
